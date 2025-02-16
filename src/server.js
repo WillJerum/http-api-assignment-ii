ed7d116aa68ef6129df99c56952acfef0b11234e
WillJerum <wrj2313@g.rit.edu>
2025-02-16 15:12:11 -0500

    Finished making changes

--- src/server.js
+++ src/server.js
@@ -7,29 +7,31 @@ const apiHandler = require('./apiResponses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const onRequest = (request, response) => {
-  console.log(request.url);
+    console.log(request.url);
 
-  const parsedUrl = url.parse(request.url, true); // Parse the URL
+    const parsedUrl = url.parse(request.url, true); // Parse the URL
 
-  switch (parsedUrl.pathname) { // Use pathname instead of full URL
-    case '/':
-      htmlHandler.getIndex(request, response);
-      break;
-    case '/style.css':
-      htmlHandler.getCss(request, response);
-      break;
-    case '/getUsers': // Corrected this line
-      apiHandler.getUsers(request, response);
-      break;
-    case '/addUser':
-      apiHandler.addUser(request, response);
-      break;
-    default:
-      apiHandler.notFound(request, response); // Handle 404
-      break;
-  }
+    switch (parsedUrl.pathname) { 
+      case '/':
+        htmlHandler.getIndex(request, response);
+        break;
+      case '/style.css':
+        htmlHandler.getCss(request, response);
+        break;
+      case '/getUsers':
+        apiHandler.getUsers(request, response);
+        break;
+      case '/addUser':
+        apiHandler.addUser(request, response);
+        break;
+      default:
+        apiHandler.notFound(request, response);
+        break;
+    }
 };
 
+
+
 http.createServer(onRequest).listen(port, () => {
-  console.log(`Listening on 127.0.0.1:${port}`);
+    console.log(`Listening on 127.0.0.1:${port}`);
 });
